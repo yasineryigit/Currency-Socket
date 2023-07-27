070d80d7b158155bc0bb4cf026fa1850ea96415d
yasineryigit <yasineryigit737@gmail.com>
2023-07-27 16:44:59 +0300

    Initial commit

---
+++ src/main/java/com/ossovita/currencysocket/CurrencySocketApplication.java
@@ -0,0 +1,14 @@
+package com.ossovita.currencysocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencySocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencySocketApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/ossovita/currencysocket/CurrencySocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ossovita.currencysocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencySocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
